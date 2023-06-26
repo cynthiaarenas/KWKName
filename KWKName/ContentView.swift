58d1a27d9b031bca5e903d60346c24dd48ce553d
Cynthia Arenas <cynthiaarenas.ca@gmail.com>
2023-06-26 11:03:39 -0700

    Fixed errors

--- KWKName/ContentView.swift
+++ KWKName/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var = name = ""
-    @State private var = textTitle = "What is your name?"
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text("What is your name???")
             TextField("", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
