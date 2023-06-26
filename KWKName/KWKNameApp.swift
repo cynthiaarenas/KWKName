498d1e662b4cfbddbeba3af0af511622eeb34d3b
Cynthia Arenas <cynthiaarenas.ca@gmail.com>
2023-06-26 10:43:13 -0700

    Initial Commit

---
+++ KWKName/KWKNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKNameApp.swift
+//  KWKName
+//
+//  Created by Cynthia Arenas on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct KWKNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
